b03f3483891a944b46ad214575f0c8c9c06f95bd
= <ldormon2@sheffield.ac.uk>
2023-11-11 17:23:18 +0000

    routing

---
+++ Home.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button.js';
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.title}>You've Got Crabs!</Text>
+          <Text> Ending crustacean frustration </Text>
+    
+          <View style={styles.buttonContainer}>
+            <Button style={{button: {backgroundColor: "black"}}} title="Login"></Button>
+            <Button style title="Sign Up"></Button>
+          </View>
+    
+          <StatusBar style="auto" />
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center', 
+      justifyContent: 'center',
+    },
+    title: {
+      fontSize: 48,
+      color: "red"
+    },
+    buttonContainer: {
+      margin: 20,
+      display: 'flex',
+      flexDirection: 'row',
+      gap: 20,
+    }
+  });
+  
