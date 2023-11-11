f37f35ab24cc418b5600f56665c0b6dd373bf972
= <ldormon2@sheffield.ac.uk>
2023-11-11 19:27:47 +0000

    add back login page header

--- App.js
+++ App.js
@@ -13,9 +13,7 @@ export default function App() {
         <Stack.Screen name="Home" component={Home} options={{
           headerShown: false
         }} />
-        <Stack.Screen name="Login" component={Login} options={{
-          headerShown: false
-        }} />
+        <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
   );
